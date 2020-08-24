7ddd73121eb35c33901555c68f5c718347677d9d
Hsien-Hsiu <hsien-hsiu.liao@yandex.com>
2020-08-23 19:37:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   
   break_val=5;
-  var audiofile = new Audio('./sounds/bells-tibetan.mp3');
+  var audiofile = new Audio('./sounds/service-bell.mp3');
   session_len=25;
   count=session_len*60;
   
